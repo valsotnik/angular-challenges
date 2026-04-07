cf81b30ddf9a3b932bd36b261463344be94d53a8
v.sotnik <cmvaltime@gmail.com>
2026-04-07 15:14:16 +0200

    feat: decouple entities with InjectionToken

--- libs/decoupling/core/src/index.ts
+++ libs/decoupling/core/src/index.ts
@@ -1,1 +1,9 @@
-export const DECOUPLING_CORE = 'DECOUPLING_CORE';
+import { InjectionToken, Signal } from '@angular/core';
+
+export type ButtonState = 'enabled' | 'disabled';
+
+export interface ButtonStatePort {
+  state: Signal<ButtonState>;
+}
+
+export const BUTTON_STATE = new InjectionToken<ButtonStatePort>('BUTTON_STATE');
